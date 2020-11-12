3a06f9281b1ca027acd7e2f0b6f4201bb93b667d
Joseph <joseph42@live.co.uk>
2020-11-12 17:18:58 +0000

    Change to spotify's authorization code flow

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ClassLister from 'css-module-class-lister';
-import { spotifyApi, getAccessToken } from './Spotify';
+import { spotifyApi, getTokenByAuthCode, refreshAccessByRefresh, redirectToLogin } from './Spotify';
 
 import styles from '../assets/style/audio.module.css';
 import Pause from '../assets/image/pause.png';
 import FastForward from '../assets/image/fast-forward.png';
 import Volume from '../assets/image/volume.png';
+import Play from '../assets/image/play.png';
 
 import Progress from './Progress';
 
 const classes = ClassLister(styles);
+const SPOTIFY_REFRESH_TOKEN_KEY = '__spotify_refresh_token';
 
 class Audio extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      code: '',
+      access_token: '',
+      refresh_token: '',
+
+      playing: false,
       percent: 0,
       volume: 0,
       track: {
@@ -27,22 +34,70 @@ class Audio extends React.Component {
     };
   }
 
-  componentDidMount = () => {
-    this.refreshToken();
-    this.refreshInterval = setInterval(this.refreshToken, 3000 * 1000)
-  };
-  
-  componentWillUnmount = () => clearInterval(this.refreshInterval);
+  componentDidMount = async () => {
+    if (localStorage.getItem(SPOTIFY_REFRESH_TOKEN_KEY) != null) {
+      console.log('spotify : using refresh token from storage');
+      await this.setRefreshToken(localStorage.getItem(SPOTIFY_REFRESH_TOKEN_KEY));
+      this.refreshAccessToken();
+      this.startAccessRefresh();
+    } else {
+      const query = new URLSearchParams(window.location.search);
+      if (query.has('code')) {
+        console.log('spotify : found code in querystring');
+        const request = await getTokenByAuthCode(query.get('code'));
+        if (request.status === 200) {
+          const data = await request.json();
+          await this.setAccessToken(data.access_token);
+          await this.setRefreshToken(data.refresh_token);
 
-  refreshToken = async () => {
-    const request = await getAccessToken();
+          this.startAccessRefresh();
+          return;
+        }
+      }
+
+      redirectToLogin();
+    }
+  }
+
+  startAccessRefresh = () => {
+    console.log('spotify : startAccessRefresh');
+    this.refreshInterval = setInterval(this.refreshAccessToken, 3000 * 1000);
+  }
+
+  refreshAccessToken = async () => {
+    console.log('spotify : refreshAccessToken');
+    const request = await refreshAccessByRefresh(this.state.refresh_token);
     if (request.status === 200) {
       const data = await request.json();
-      spotifyApi.setAccessToken(data.access_token);
-      console.log('Access token set successfully');
+      await this.setAccessToken(data.access_token);
     } else {
-      console.error(`Unable to fetch spotify access token: ${request.statusText}, retrying in 5 seconds...`);
-      setTimeout(this.refreshToken, 5 * 1000);
+      console.log('spotify : could not refresh using refresh token, starting again...');
+      localStorage.removeItem(SPOTIFY_REFRESH_TOKEN_KEY);
+      redirectToLogin();
+    }
+  }
+
+  componentWillUnmount = () => clearInterval(this.refreshInterval);
+
+  setAccessToken = async (token) => {
+    console.log('spotify : setAccessToken');
+    spotifyApi.setAccessToken(token);
+    await this.setState({ access_token: token });
+  }
+
+  setRefreshToken = async (token) => {
+    console.log('spotify : setRefreshToken');
+    localStorage.setItem(SPOTIFY_REFRESH_TOKEN_KEY, token);
+    await this.setState({ refresh_token: token });
+  }
+
+  handleStateControl = async () => {
+    if (this.state.playing) {
+      await spotifyApi.pause();
+      this.setState({ playing: false });
+    } else {
+      await spotifyApi.play();
+      this.setState({ playing: true });
     }
   }
 
@@ -65,7 +120,7 @@ class Audio extends React.Component {
           </div>
 
           <div className={classes('control', 'control-mid')}>
-            <img className={classes('statecontrol', 'statecontrol-mid')} src={Pause} alt="Pause / play icon" />
+            <img className={classes('statecontrol', 'statecontrol-mid')} src={this.state.playing ? Pause : Play} onClick={this.handleStateControl} alt="Pause / play icon" />
           </div>
 
           <div className={styles.control}>
--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -3,16 +3,47 @@ import SpotifyWebApi from 'spotify-web-api-js';
 const spotifyApi = new SpotifyWebApi();
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
+const scope = process.env.REACT_APP_SPOTIFY_SCOPE;
+const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
 
 export { spotifyApi };
 
-export const getAccessToken = () => fetch('https://accounts.spotify.com/api/token', {
-  method: 'POST',
-  headers: {
+const createHeaders = () => {
+  return {
     'Authorization': `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
     'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  body: [
-    `${encodeURIComponent('grant_type')}=${encodeURIComponent('client_credentials')}`
-  ]
-});
+  };
+}
+
+export const getTokenByAuthCode = (code) => {
+  const body = {
+    grant_type: 'authorization_code',
+    code: code,
+    redirect_uri: redirectUri
+  };
+
+  return fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: createHeaders(),
+    body: Object.keys(body).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(body[key])).join('&')
+  });
+}
+
+export const refreshAccessByRefresh = (refreshToken) => {
+  const body = {
+    grant_type: 'refresh_token',
+    refresh_token: refreshToken
+  };
+  
+  return fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: createHeaders(),
+    body: Object.keys(body).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(body[key])).join('&')
+  });
+}
+
+export const redirectToLogin = () => window.location.href = 'https://accounts.spotify.com/authorize'
+                                                            + '?response_type=code'
+                                                            + `&client_id=${encodeURIComponent(clientId)}`
+                                                            + `&scope=${encodeURIComponent(scope)}`
+                                                            + `&redirect_uri=${encodeURIComponent(redirectUri)}`;
