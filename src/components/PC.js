d44eef40cb79e779222c39c3055233104084a59b
Joseph <joseph42@live.co.uk>
2020-11-12 03:56:37 +0000

    Add memory and disk

--- src/components/PC.js
+++ src/components/PC.js
@@ -38,6 +38,14 @@ class PC extends React.Component {
         name: '...',
         data: []
       },
+      memory: {
+        name: '...',
+        data: []
+      },
+      disk: {
+        name: '...',
+        data: []
+      },
     };
   }
 
@@ -102,6 +110,76 @@ class PC extends React.Component {
 
                 <div className={styles.information_value_small}>
                   <div>0c</div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.chart}>
+              <ChartistGraph className="ct-double-octave" data={{
+                series: [
+                  { data: this.state.gpu.data }
+                ]
+              }} 
+              type={'Line'} 
+              options={{
+                high: 100,
+                low: 0,
+                ...CHART_OPTIONS
+              }} />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={classes('stat', 'stat_two')}>
+            <div className={styles.information}>
+              <div className={styles.information_name}>
+                <div className={styles.information_name_title}>
+                  <div>Memory</div>
+                </div>
+
+                <div className={styles.information_name_brand}>
+                  <div>{this.state.memory.name}</div>
+                </div>
+              </div>
+
+              <div className={styles.information_value}>
+                <div className={styles.information_value_full}>
+                  <div>0/0 GB</div>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.chart}>
+              <ChartistGraph className="ct-double-octave" data={{
+                series: [
+                  { data: this.state.cpu.data }
+                ]
+              }} 
+              type={'Line'} 
+              options={{
+                high: 100,
+                low: 0,
+                ...CHART_OPTIONS
+              }} />
+            </div>
+          </div>
+
+          <div className={classes('stat', 'stat_two')}>
+            <div className={styles.information}>
+              <div className={styles.information_name}>
+                <div className={styles.information_name_title}>
+                  <div>Disk</div>
+                </div>
+
+                <div className={styles.information_name_brand}>
+                  <div>{this.state.disk.name}</div>
+                </div>
+              </div>
+
+              <div className={styles.information_value}>
+                <div className={styles.information_value_full}>
+                  <div>0%</div>
                 </div>
               </div>
             </div>
