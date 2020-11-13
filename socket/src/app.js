e66d69d86ab8430a9857bb5ffbaaf6dc766be1d4
Joseph <joseph42@live.co.uk>
2020-11-13 13:52:32 +0000

    Add socket events

--- socket/src/app.js
+++ socket/src/app.js
@@ -1,6 +1,39 @@
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: 51146 });
 
-wss.on('connection', ws => {
-  ws.on('message', message => console.log(`Received message => ${message}`));
+const channelMessageRegex = /^([a-zA-Z0-9]+)((:([a-zA-Z0-9]+))+)$/;
+const events = {};
+
+const addSocketEvent = (channel, func) => {
+  const attached = [];
+  if (events[channel]) {
+    attached = events[channel];
+  }
+
+  attached.push(func);
+  events[channel] = attached;
+};
+
+addSocketEvent('authenticate', (socket, value, type) => {
+  console.log(`authenticate = ${value} ${type}`);
 });
+
+const handleSocketMessage = (message, ws) => {
+  if (message.length <= 500 && message.match(channelMessageRegex)) {
+    const args = message.split(':');
+    const channel = args[0];
+    args.shift();
+
+    if (events[channel]) {
+      for (const callback of events[channel]) {
+        callback(ws, ...args);
+      }
+    }
+  }
+};
+
+const handleSocketConnection = (ws) => {
+  ws.on('message', message => handleSocketMessage(message, ws));
+};
+
+wss.on('connection', handleSocketConnection);
