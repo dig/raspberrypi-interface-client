ab20373a446221dfee7311393b810cbfcf437292
Joseph <joseph42@live.co.uk>
2020-11-13 14:10:24 +0000

    Add socket auth

--- socket/src/app.js
+++ socket/src/app.js
@@ -1,8 +1,11 @@
-const WebSocket = require('ws')
-const wss = new WebSocket.Server({ port: 51146 });
+require('dotenv').config({ path: '../.env' });
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: process.env.SOCKET_PORT });
 
 const channelMessageRegex = /^([a-zA-Z0-9]+)((:([a-zA-Z0-9]+))+)$/;
 const events = {};
+let connectionCount = 0;
 
 const addSocketEvent = (channel, func) => {
   const attached = [];
@@ -15,7 +18,11 @@ const addSocketEvent = (channel, func) => {
 };
 
 addSocketEvent('authenticate', (socket, value, type) => {
-  console.log(`authenticate = ${value} ${type}`);
+  if (value === process.env.SOCKET_AUTH_KEY) {
+    socket.authenticated = true;
+    socket.clientType = type;
+    console.log(`${socket.id} authenicated!`);
+  }
 });
 
 const handleSocketMessage = (message, ws) => {
@@ -24,7 +31,7 @@ const handleSocketMessage = (message, ws) => {
     const channel = args[0];
     args.shift();
 
-    if (events[channel]) {
+    if (events[channel] && (socket.authenticated || channel === 'authenticate')) {
       for (const callback of events[channel]) {
         callback(ws, ...args);
       }
@@ -33,6 +40,7 @@ const handleSocketMessage = (message, ws) => {
 };
 
 const handleSocketConnection = (ws) => {
+  ws.id = connectionCount++;
   ws.on('message', message => handleSocketMessage(message, ws));
 };
 
